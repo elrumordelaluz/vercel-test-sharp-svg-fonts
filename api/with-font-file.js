a92d4a7928927525c3565b3c1889c60d7817f674
Lionel <elrumordelaluz@hotmail.com>
2021-04-27 09:47:03 +0200

    Complete examples

---
+++ api/with-font-file.js
@@ -0,0 +1,78 @@
+import fetch from 'node-fetch'
+import sharp from 'sharp'
+import path from 'path'
+
+path.resolve(process.cwd(), 'fonts', 'fonts.conf')
+path.resolve(process.cwd(), 'fonts', 'Arial Bold.ttf')
+
+export default async function handler(req, res) {
+  try {
+    const url = `https://images.unsplash.com/photo-1475965894430-b05c9d13568a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&fm=jpg&fit=crop&w=1080&q=80&fit=max`
+    const response = await fetch(url)
+    const bgImg = await response.buffer()
+
+    const mainText = Buffer.from(
+      `<svg width="500" height="200">
+          <style type="text/css">
+            @font-face {
+              font-family: Arial Bold;
+              src: './fonts/Arial Bold.ttf'';
+            }
+          </style>
+          <text x="0" y="0" font-family="Arial Bold" font-size="48" font-weight="bold" letter-spacing="-0.7">
+            <tspan x="0" dy="1.2em">Lorem Ipsum</tspan>
+            <tspan x="0" dy="1.2em" dx=".4em" fill="#6356fd">dolor sit</tspan> <tspan dx=".05em">amet.</tspan>
+          </text>
+        </svg>`
+    )
+
+    const smallText = Buffer.from(
+      `<svg width="870" height="50">
+          <text x="50%" dominant-baseline="middle" text-anchor="middle" y="22" font-family="Arial Bold'" font-size="22" font-weight="bold" fill="#2c2d2c" letter-spacing=".8">
+            Hello World.
+          </text>
+        </svg>`
+    )
+
+    const mainTextBuff = await sharp(mainText)
+      .extend({
+        top: 200,
+        background: { r: 0, g: 0, b: 0, alpha: 0 },
+      })
+      .png()
+      .toBuffer()
+
+    const smallTextBuff = await sharp(smallText)
+      .extend({
+        bottom: 200,
+        background: { r: 0, g: 0, b: 0, alpha: 0 },
+      })
+      .png()
+      .toBuffer()
+
+    const bg = await sharp(bgImg)
+      .composite([
+        {
+          input: mainTextBuff,
+          gravity: 'north',
+        },
+        {
+          input: smallTextBuff,
+          gravity: 'south',
+        },
+      ])
+      .png()
+      .toBuffer()
+
+    res.setHeader('Content-Type', `image/png`)
+    res.setHeader(
+      'Cache-Control',
+      `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`
+    )
+    res.statusCode = 200
+    res.end(bg)
+  } catch (err) {
+    console.log({ err })
+    res.status(404).send('Not Found')
+  }
+}
